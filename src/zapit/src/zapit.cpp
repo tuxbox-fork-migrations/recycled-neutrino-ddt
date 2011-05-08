tuxbox-fork-migrations/recycled-neutrino-ddt

commit 93eb9328a3161815dc0b149b7b3ea931bed549b9

neutrino/zapit: don't start video when leaving standby
Parental handling was defeated because zapit did start video before
a check for lockstatus could be done.
Fix this by:
* not zapping to the current channel in leaveStandby() in zapit
* explicitly zapping during start and after leaving standby from
 neutrino

git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@1263 e54a6e83-5905-42d5-8d5c-058d10e6a962


Origin commit data
------------------
Branch: master
Commit: https://github.com/Duckbox-Developers/neutrino-ddt/commit/3aa7e2f854d8cb852b14873f67511b4a72fb14c5
Author: Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
Date: 2011-03-06 (Sun, 06 Mar 2011)

Origin message was:
------------------
neutrino/zapit: don't start video when leaving standby

Parental handling was defeated because zapit did start video before
a check for lockstatus could be done.
Fix this by:
* not zapping to the current channel in leaveStandby() in zapit
* explicitly zapping during start and after leaving standby from
 neutrino

git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@1263 e54a6e83-5905-42d5-8d5c-058d10e6a962


------------------
This commit was generated by Migit

src/zapit/src/zapit.cpp
@@ -404,6 +404,7 @@ printf("[zapit] saving channel, apid %x sub pid %x mode %d volume %d\n", g_curre
                 audio_map[g_current_channel->getChannelID()].volume = audioDecoder->getVolume();
                 audio_map[g_current_channel->getChannelID()].subpid = dvbsub_getpid();
 		tuxtx_subtitle_running(&audio_map[g_current_channel->getChannelID()].ttxpid, &audio_map[g_current_channel->getChannelID()].ttxpage, NULL);
+		firstzap = false;
         }
 
 	if (in_nvod) {
@@ -2087,8 +2088,10 @@ void leaveStandby(void)
 	}
 
 	standby = false;
+#if 0
 	if (g_current_channel)
 		zapit(live_channel_id, current_is_nvod, false, true);
+#endif
 }
 
 unsigned zapTo(const unsigned int bouquet, const unsigned int pchannel)
@@ -2264,7 +2267,7 @@ int zapit_main_thread(void *data)
 	// sleep(1) is ok here. (striper)
 	sleep(1);
 	leaveStandby();
-	firstzap = false;
+	//firstzap = false;
 	stime = time(0);
 	//time_t curtime;
 	pthread_create (&tsdt, NULL, sdt_thread, (void *) NULL);

src/neutrino.cpp
@@ -1980,11 +1980,11 @@ void CNeutrinoApp::InitZapper()
 	channelsInit();
 
 	if(firstchannel.mode == 't') {
-		tvMode(false);
+		tvMode(true);
 	} else {
 		g_RCInput->killTimer(g_InfoViewer->lcdUpdateTimer);
 		g_InfoViewer->lcdUpdateTimer = g_RCInput->addTimer( LCD_UPDATE_TIME_RADIO_MODE, false );
-		radioMode(false);
+		radioMode(true);
 	}
 	if(g_settings.cacheTXT)
 		tuxtxt_init();
@@ -3985,6 +3985,10 @@ void CNeutrinoApp::standbyMode( bool bOnOff )
 		g_Zapit->setStandby(false);
 		if(was_record)
 			g_Zapit->startPlayBack();
+		else {
+			channelList->setSelected(0xfffffff); /* make sure that zapTo_ChannelID will zap */
+			channelList->zapTo_ChannelID(live_channel_id);
+		}
 		if(recordingstatus) was_record = 0;
 
 		videoDecoder->Standby(false);

commit 0343d3077e32ab10033112df9fd9a9b314a1d5ba

zapit: fix saving of current audio pid (got broken with r1263)
git-svn-id: file:///home/bas/coolstream_public_svn/THIRDPARTY/applications/neutrino-experimental@1447 e54a6e83-5905-42d5-8d5c-058d10e6a962


Origin commit data
------------------
Commit: https://github.com/Duckbox-Developers/neutrino-ddt/commit/39c94199bf0f7847575ea0fd86c607cdb18249fa
Author: Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
Date: 2011-05-08 (Sun, 08 May 2011)



------------------
This commit was generated by Migit

src/zapit/src/zapit.cpp
@@ -404,8 +404,9 @@ printf("[zapit] saving channel, apid %x sub pid %x mode %d volume %d\n", g_curre
                 audio_map[g_current_channel->getChannelID()].volume = audioDecoder->getVolume();
                 audio_map[g_current_channel->getChannelID()].subpid = dvbsub_getpid();
 		tuxtx_subtitle_running(&audio_map[g_current_channel->getChannelID()].ttxpid, &audio_map[g_current_channel->getChannelID()].ttxpage, NULL);
-		firstzap = false;
         }
+	/* firstzap right now does nothing but control saving the audio channel */
+	firstzap = false;
 
 	if (in_nvod) {
 		current_is_nvod = true;