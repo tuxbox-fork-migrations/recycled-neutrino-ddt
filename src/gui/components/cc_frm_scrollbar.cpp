tuxbox-fork-migrations/recycled-neutrino-ddt

commit 1df3ceb11f91c8032238df2b55670cd13d5e8a24

cc_frm_scrollbar.cpp icon dont need full path
Origin commit data
------------------
Commit: https://github.com/Duckbox-Developers/neutrino-ddt/commit/10c430d40e2b537882941c14f41f57724e4a87ba
Author: Jacek Jendrzej <overx300@gmail.com>
Date: 2016-02-16 (Tue, 16 Feb 2016)


------------------
No further description and justification available within origin commit message!

------------------
This commit was generated by Migit

src/gui/components/cc_frm_scrollbar.cpp
@@ -81,8 +81,8 @@ void CComponentsScrollBar::initVarSbForm(const int& count)
 	sb_segments_obj = NULL;
 
 	string ftype = ".png";
-	sb_up_icon	= frameBuffer->getIconBasePath() + "/" + NEUTRINO_ICON_BUTTON_TOP + ftype;
-	sb_down_icon	= frameBuffer->getIconBasePath() + "/" + NEUTRINO_ICON_BUTTON_DOWN + ftype;
+	sb_up_icon	= NEUTRINO_ICON_BUTTON_TOP + ftype;
+	sb_down_icon	= NEUTRINO_ICON_BUTTON_DOWN + ftype;
 
 	sb_segments_count = count;
 	sb_mark_id	= 0;

commit bb5b74cb27d64b52c07b2e688fd5966e615d7a1f

CComponentsScrollBar: fix missed navi arrows
Paths were broken
comes in with 1df3ceb11f91c8032238df2b55670cd13d5e8a24


Origin commit data
------------------
Commit: https://github.com/Duckbox-Developers/neutrino-ddt/commit/c07f9994761406e495fb58778399b14720156678
Author: Thilo Graf <dbt@novatux.de>
Date: 2016-03-17 (Thu, 17 Mar 2016)



------------------
This commit was generated by Migit

src/gui/components/cc_frm_scrollbar.cpp
@@ -80,9 +80,8 @@ void CComponentsScrollBar::initVarSbForm(const int& count)
 	sb_up_obj 	= sb_down_obj = NULL;
 	sb_segments_obj = NULL;
 
-	string ftype = ".png";
-	sb_up_icon	= NEUTRINO_ICON_BUTTON_TOP + ftype;
-	sb_down_icon	= NEUTRINO_ICON_BUTTON_DOWN + ftype;
+	sb_up_icon	= frameBuffer->getIconPath(NEUTRINO_ICON_BUTTON_TOP) ;
+	sb_down_icon	= frameBuffer->getIconPath(NEUTRINO_ICON_BUTTON_DOWN);
 
 	sb_segments_count = count;
 	sb_mark_id	= 0;